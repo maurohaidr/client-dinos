7b51b9f24eede8085da9ef4a158c0003be64c1c5
maurohaidr <maurohaidr@gmailcom>
2021-06-25 16:34:50 -0300

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -127,7 +127,7 @@ return(
             )
       })
       : props.dinos && props.dinos.length < 1? <div className='dinoCardNotFound'>
-        <img className='imgCard' src='https://i.gifer.com/1FEk.gif' width="360" height="240" alt="" />
+        <img className='imgCard' src='https://i.gifer.com/1FEk.gif' width="300" height="200" alt="" />
         <span className='textCardNotFound'>Sorry, but we couldn't find any dino to match your search!</span>
         </div> 
       : searching &&
